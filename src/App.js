2fffebda9e1b50a3a90126591557615a252e3820
seulGit <seulgi9421@gmail.com>
2022-12-29 13:53:09 +0900

    221229 리액트 신규

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Header(props) {
+  console.log('props', props, props.title);
+  return <header>
+    <h1><a href='/' onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+
+function Nav(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t= props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read'+t.id} onClick={event=>{
+      event.preventDefault();
+      props.onChangeMode(t.id);
+    }}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+
+function Article(props) {
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+}
 
 function App() {
+  const _mode = useState('WELCOME');
+  console.log('_mode', _mode);
+  const topics = [
+    {id:1, title:'html', body:'html  is...'},
+    {id:2, title:'css', body:'css  is...'},
+    {id:3, title:'javascript', body:'javascript  is...'}
+  ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ') {
+    content = <Article title="Read" body="Hello, Read"></Article>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="WEB" onChangeMode={()=>{
+        mode = 'WELCOME';
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        mode = 'READ';
+      }}></Nav>
+      {content}
     </div>
   );
 }
